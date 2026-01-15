1f44f40492414a3033aeeeed7b73593f09aa4f51
ngozzi <nic.gozzi@gmail.com>
2026-01-15 16:44:23 +0100

    fixing header

--- layout/header.py
+++ layout/header.py
@@ -2,16 +2,15 @@ import streamlit as st
 from helpers import data_uri
 
 def show_dashboard_header():
+    epyscenario = data_uri("assets/epyscenario.svg")
     epydemix = data_uri("assets/epydemix-icon.svg")
     st.markdown(f"""
                 <style>
                 .title-row {{ display:flex; align-items:center; gap:8px; }}
-                .title-row img {{ height:58px; }}
-                .title-row h1 {{ margin:0; font-weight:700; }}
+                .title-row img {{ height:158px; }}
                 </style>
                 <div class="title-row">
-                <img src="{epydemix}" alt="Epydemix">
-                <h1><span style="color:#FFAE42;">EpyScenario</span> Dashboard</h1>
+                <img src="{epyscenario}" alt="EpyScenario Dashboard">
                 </div>
                 """, unsafe_allow_html=True)
     st.caption("Build Scenarios. Run Simulations. Explore Insights.")
